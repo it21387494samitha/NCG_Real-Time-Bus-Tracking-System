f1ffc944f152249688c727721feb3cde974783a8
vinuvi-thalagala <vinuvithalagala01@outlook.com>
2026-01-08 10:01:11 +0530

    updated the bus model

--- Backend/models/bus.model.js
+++ Backend/models/bus.model.js
@@ -3,13 +3,9 @@ import mongoose from "mongoose";
 const busShcema = new mongoose.Schema({
 
     busNumber : {type : String, required : true, unique : true},
-    driverName : {type : String, required : true},
-    routeNumber : {type : mongoose.Schema.Types.ObjectId, ref : "Route", required : true},
-    currentLocation : {
-        longitude : {type : Number, required : true},
-        latitude : {type : Number, required : true},
-        timestamp : {type : Date}
-    },
+    model : {type : String, required : true},
+    permitId : {type : mongoose.Schema.Types.ObjectId, ref : 'Permit', required : true},
+    seats : {type : Number, required : true},
     status : {type : String, enum : ['active', 'inactive'], default : 'inactive' }
 
 }, {timestamps : true});
